7a178f7c4071fc727188ac6371f91e049718801a
roger.issonaga@gmail.com <roger.issonaga@gmail.com>
2023-04-05 22:53:34 +0000

    Criamos os % de meios de pagamento

---
+++ src/etl/pagamentos.sql
@@ -0,0 +1,108 @@
+-- Databricks notebook source
+-- DBTITLE 1, 
+WITH tb_join
+AS (
+	SELECT t2.*
+		,t3.idVendedor
+	FROM silver.olist.pedido AS t1
+	LEFT JOIN silver.olist.pagamento_pedido AS t2 ON t1.idPedido = t2.idPedido
+	LEFT JOIN silver.olist.item_pedido AS t3 ON t1.idPedido = t3.idPedido
+	WHERE t1.dtPedido < '2018-01-01'
+		AND t1.dtPedido >= add_months('2018-01-01', - 6)
+		AND t3.idVendedor IS NOT NULL
+	)
+	,tb_group
+AS (
+	SELECT idVendedor
+		,descTipoPagamento
+		,count(DISTINCT idPedido) AS qtdePedidoMeioPagamento
+		,sum(vlPagamento) AS vlPedidoMeioPagamento
+	FROM tb_join
+	GROUP BY idVendedor
+		,descTipoPagamento
+	ORDER BY idVendedor
+		,descTipoPagamento
+	)
+SELECT idVendedor
+	,sum(CASE 
+			WHEN descTipoPagamento = 'boleto'
+				THEN qtdePedidoMeioPagamento
+			ELSE 0
+			END) AS qtde_boleto_pedido
+	,sum(CASE 
+			WHEN descTipoPagamento = 'credit_card'
+				THEN qtdePedidoMeioPagamento
+			ELSE 0
+			END) AS qtde_credit_card_pedido
+	,sum(CASE 
+			WHEN descTipoPagamento = 'voucher'
+				THEN qtdePedidoMeioPagamento
+			ELSE 0
+			END) AS qtde_voucher_pedido
+	,sum(CASE 
+			WHEN descTipoPagamento = 'debit_card'
+				THEN qtdePedidoMeioPagamento
+			ELSE 0
+			END) AS qtde_debit_card_pedido
+    ,sum(CASE 
+			WHEN descTipoPagamento = 'boleto'
+				THEN vlPedidoMeioPagamento
+			ELSE 0
+			END) AS vl_boleto_pedido
+	,sum(CASE 
+			WHEN descTipoPagamento = 'credit_card'
+				THEN vlPedidoMeioPagamento
+			ELSE 0
+			END) AS vl_credit_card_pedido
+	,sum(CASE 
+			WHEN descTipoPagamento = 'voucher'
+				THEN vlPedidoMeioPagamento
+			ELSE 0
+			END) AS vl_voucher_pedido
+	,sum(CASE 
+			WHEN descTipoPagamento = 'debit_card'
+				THEN vlPedidoMeioPagamento
+			ELSE 0
+			END) AS vl_debit_card_pedido
+      ,sum(CASE 
+			WHEN descTipoPagamento = 'boleto'
+				THEN qtdePedidoMeioPagamento
+			ELSE 0
+			END) / sum(qtdePedidoMeioPagamento) AS pct_qtde_boleto_pedido
+	,sum(CASE 
+			WHEN descTipoPagamento = 'credit_card'
+				THEN qtdePedidoMeioPagamento
+			ELSE 0
+			END) / sum(qtdePedidoMeioPagamento) AS pct_qtde_credit_card_pedido
+	,sum(CASE 
+			WHEN descTipoPagamento = 'voucher'
+				THEN qtdePedidoMeioPagamento
+			ELSE 0
+			END) / sum(qtdePedidoMeioPagamento) AS pct_qtde_voucher_pedido
+	,sum(CASE 
+			WHEN descTipoPagamento = 'debit_card'
+				THEN qtdePedidoMeioPagamento
+			ELSE 0
+			END) / sum(qtdePedidoMeioPagamento) AS pct_qtde_debit_card_pedido
+      ,sum(CASE 
+			WHEN descTipoPagamento = 'boleto'
+				THEN vlPedidoMeioPagamento
+			ELSE 0
+			END) / sum(vlPedidoMeioPagamento) AS pct_vl_boleto_pedido
+	,sum(CASE 
+			WHEN descTipoPagamento = 'credit_card'
+				THEN vlPedidoMeioPagamento
+			ELSE 0
+			END) / sum(vlPedidoMeioPagamento) AS pct_vl_credit_card_pedido
+	,sum(CASE 
+			WHEN descTipoPagamento = 'voucher'
+				THEN vlPedidoMeioPagamento
+			ELSE 0
+			END) / sum(vlPedidoMeioPagamento) AS pct_vl_voucher_pedido
+	,sum(CASE 
+			WHEN descTipoPagamento = 'debit_card'
+				THEN vlPedidoMeioPagamento
+			ELSE 0
+			END) / sum(vlPedidoMeioPagamento) AS pct_vl_debit_card_pedido
+FROM tb_group
+GROUP BY 1
