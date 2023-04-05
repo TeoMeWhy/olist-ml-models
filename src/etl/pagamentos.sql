c27bd2099c421d026ce1853dd69efff58d3cfdd7
andremkrul@gmail.com <andremkrul@gmail.com>
2023-04-05 23:31:51 +0000

    ETL Pagamento Completo

--- src/etl/pagamentos.sql
+++ src/etl/pagamentos.sql
@@ -1,26 +1,42 @@
 -- Databricks notebook source
-select *
-from silver.olist.pedido_pagamento
-
--- COMMAND ----------
-
-with tb_join as(
 select 
-t2.*,
-t3.idvendedor
-
+distinct
+t1.idPedido,
+t2.idVendedor
 
 from silver.olist.pedido as t1
 
-left join silver.olist.pagamento_pedido as t2
+left join silver.olist.item_pedido as t2
 on t1.idpedido = t2.idpedido
 
-left join silver.olist.item_pedido as t3
-on t1.idpedido = t3.idpedido
+where t1.dtpedido < '2018-01-01' and t1.dtpedido >= add_months('2018-01-01', -6)
 
-where dtpedido < '2018-01-01' and dtpedido >= add_months('2018-01-01', -6)
-and t3.idvendedor is not null
+-- COMMAND ----------
+
+with tb_pedidos as(
+select 
+distinct
+t1.idPedido,
+t2.idVendedor
+
+from silver.olist.pedido as t1
+
+left join silver.olist.item_pedido as t2
+on t1.idpedido = t2.idpedido
+
+where t1.dtpedido < '2018-01-01' and t1.dtpedido >= add_months('2018-01-01', -6)
+and idVendedor is not null
 ),
+
+tb_join as(
+select 
+t2.*,
+t1.idVendedor
+from tb_pedidos as t1
+left join silver.olist.pagamento_pedido as t2
+on t1.idpedido = t2.idpedido
+),
+
 tb_group as(
 select 
 idvendedor, 
@@ -30,7 +46,9 @@ sum(vlpagamento) as vl_pedido_meio_pagamento
 from tb_join
 
 group by idvendedor, desctipopagamento
-)
+),
+
+tb_summary as(
 select 
 idvendedor,
 sum(case when desctipopagamento =  'boleto' then qtd_pedido_meio_pagamento else 0 end) as qtd_boleto,
@@ -54,3 +72,28 @@ sum(case when desctipopagamento =  'voucher' then vl_pedido_meio_pagamento else 
 sum(case when desctipopagamento =  'debit_card' then vl_pedido_meio_pagamento else 0 end) / sum(vl_pedido_meio_pagamento) as prop_qtd_vl_debit_card
 from tb_group
 group by idvendedor
+),
+tb_cartao as(
+select
+idVendedor,
+avg(nrParcelas) avgQtdParcelas,
+percentile(nrParcelas, 0.5) as medianQtdParcelas,
+max(nrParcelas) as max_parcelas,
+min(nrParcelas) as min_parcelas
+
+from tb_join
+
+where descTipoPagamento = 'credit_card'
+
+group by idVendedor
+)
+select
+'2018-01-01' as dtReference,
+t1.*,
+t2.*
+from tb_summary as t1
+
+left join tb_cartao as t2
+on t1.idvendedor = t2.idvendedor
+
+
