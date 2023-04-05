c96ed83f8fa3c6c3030dd0a86745663aeb5ea95b
igors.costa182@gmail.com <igors.costa182@gmail.com>
2023-04-05 23:35:24 +0000

    ETL de pagamentos completa

--- src/ETL/pagamentos.sql
+++ src/ETL/pagamentos.sql
@@ -1,22 +1,48 @@
 -- Databricks notebook source
-WITH tabela_consolida AS 
+SELECT 
+       DISTINCT
+       T1.idPedido
+      ,T2.idVendedor
 
-(
-      SELECT
 
-             T2.*
-            ,T3.idVendedor
+FROM silver.olist.pedido AS T1
 
-      FROM silver.olist.pedido AS T1
+LEFT JOIN silver.olist.item_pedido  AS T2 ON T1.idPedido = T2.idPedido
 
-      LEFT JOIN silver.olist.pagamento_pedido  AS T2 ON T1.idPedido = T2.idPedido
-      LEFT JOIN silver.olist.item_pedido       AS T3 ON T1.idPedido = T3.idPedido
+WHERE T1.dtPedido < '2018-01-01' AND T1.dtPedido >= add_months('2018-01-01', -6)   AND   T2.idVendedor IS NOT NULL
 
-      WHERE T1.dtPedido < '2018-01-01' AND T1.dtPedido >= add_months('2018-01-01', -6) 
-      AND   T3.idVendedor IS NOT NULL
- ),
+-- COMMAND ----------
 
- 
+WITH tb_pedidos AS (
+
+  SELECT 
+      DISTINCT 
+      t1.idPedido,
+      t2.idVendedor
+
+  FROM silver.olist.pedido AS t1
+
+  LEFT JOIN silver.olist.item_pedido as t2
+  ON t1.idPedido = t2.idPedido
+
+  WHERE t1.dtPedido < '2018-01-01'
+  AND t1.dtPedido >= add_months('2018-01-01', -6)
+  AND idVendedor IS NOT NULL
+
+),
+
+tb_join AS (
+
+  SELECT 
+        t1.idVendedor,
+        t2.*         
+
+  FROM tb_pedidos AS t1
+
+  LEFT JOIN silver.olist.pagamento_pedido AS t2
+  ON t1.idPedido = t2.idPedido
+
+),
  
  
 tabela_agrupada AS
@@ -30,12 +56,14 @@ SELECT
         ,SUM(vlPagamento)         AS vlPagamentoMeioPagamento
         ,SUM(nrParcelas) AS Parcelas
         
-FROM tabela_consolida  
+FROM tb_join
 GROUP BY idVendedor, descTipoPagamento
 ORDER BY idVendedor, descTipoPagamento
 
 
-)
+),
+
+tb_summary AS (
 
 SELECT  
       idVendedor
@@ -59,27 +87,38 @@ SELECT
      ,SUM(CASE WHEN descTipoPagamento = 'voucher'     THEN vlPagamentoMeioPagamento ELSE 0 END) / SUM(vlPagamentoMeioPagamento) AS pct_valor_pedido_Voucher
      ,SUM(CASE WHEN descTipoPagamento = 'debit_card'  THEN vlPagamentoMeioPagamento ELSE 0 END) / SUM(vlPagamentoMeioPagamento) AS pct_valor_pedido_DebitCard
       
-     ,SUM(CASE WHEN descTipoPagamento = 'credit_card' THEN qtdPedidoMeioPagamento ELSE 0 END) / SUM(CASE WHEN descTipoPagamento = 'credit_card' THEN Parcelas ELSE 0 END) AS qntd_media_parcelas_cartao_credito
      
 FROM tabela_agrupada
 GROUP BY idVendedor
 
+),
 
--- COMMAND ----------
+tb_cartao as (
 
+  SELECT idVendedor,
+         AVG(nrParcelas) AS avgQtdeParcelas,
+         PERCENTILE(nrParcelas, 0.5) AS medianQtdeParcelas,
+         MAX(nrParcelas) AS maxQtdeParcelas,
+         MIN(nrParcelas) AS minQtdeParcelas
 
+  FROM tb_join
 
--- COMMAND ----------
+  WHERE descTipoPagamento = 'credit_card'
+
+  GROUP BY idVendedor
+
+)
 
 SELECT 
-         DATE(dtPedido) as data_pedido
-        ,COUNT(*)       as pedidos
-        
-FROM silver.olist.pedido 
+       '2018-01-01' AS dtReference,
+       t1.*,
+       t2.avgQtdeParcelas,
+       t2.medianQtdeParcelas,
+       t2.maxQtdeParcelas,
+       t2.minQtdeParcelas
 
-GROUP BY 1
-ORDER BY 1
+FROM tb_summary as t1
 
--- COMMAND ----------
-
+LEFT JOIN tb_cartao as t2
+ON t1.idVendedor = t2.idVendedor
 
