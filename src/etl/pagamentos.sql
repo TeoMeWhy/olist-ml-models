c4098e074f2aa1b8e100f51792eb1b3edcca8e90
cauapablo.carvalho@gmail.com <cauapablo.carvalho@gmail.com>
2023-04-05 11:15:20 +0000

    commit-pagamentos

---
+++ src/etl/pagamentos.sql
@@ -0,0 +1,63 @@
+-- Databricks notebook source
+SELECT *
+FROM silver.olist.pagamentos_pedido
+
+-- COMMAND ----------
+
+SELECT date(dtPedido) as dtPedido,
+count(*) as qtPedido
+FROM silver.olist.pedido
+GROUP BY 1
+ORDER BY 1
+
+-- COMMAND ----------
+
+SELECT *
+FROM silver.olist.pedido
+WHERE dtPedido < '2018-01-01'
+AND dtPedido >= add_months('2018-01-01',6)
+
+-- COMMAND ----------
+
+WITH tb_join AS(
+SELECT t2.*,t3.idVendedor
+FROM silver.olist.pedido as t1
+
+LEFT JOIN silver.olist.pagamento_pedido as t2
+ON t1.idPedido = t2.idPedido
+
+LEFT JOIN silver.olist.item_pedido as t3
+ON t1.idPedido = t3.idPedido
+
+WHERE t1.dtPedido < '2018-01-01'
+AND t1.dtPedido>= add_months('2018-01-01',-6)
+),
+
+tb_group as  (
+
+SELECT 
+  idVendedor,
+
+  sum(case when descTipoPagamento='boleto' then qtdePedidoMeioPagamento else 0 end) as qtde_boleto_pedido,
+  sum(case when descTipoPagamento='credit_card' then qtdePedidoMeioPagamento else 0 end) as qtde_credit_card_pedido,
+  sum(case when descTipoPagamento='voucher' then qtdePedidoMeioPagamento else 0 end) as qtde_voucher_pedido,
+  sum(case when descTipoPagamento='debit_card' then qtdePedidoMeioPagamento else 0 end) as qtde_debit_card_pedido,
+
+  sum(case when descTipoPagamento='boleto' then vlPedidoMeioPagamento else 0 end) as valor_boleto_pedido,
+  sum(case when descTipoPagamento='credit_card' then vlPedidoMeioPagamento else 0 end) as valor_credit_card_pedido,
+  sum(case when descTipoPagamento='voucher' then vlPedidoMeioPagamento else 0 end) as valor_voucher_pedido,
+  sum(case when descTipoPagamento='debit_card' then vlPedidoMeioPagamento else 0 end) as valor_debit_card_pedido,
+  
+  sum(case when descTipoPagamento='boleto' then qtdePedidoMeioPagamento else 0 end) / sum(qtdePedidoMeioPagamento) as pct_qtd_boleto_pedido,
+  sum(case when descTipoPagamento='credit_card' then qtdePedidoMeioPagamento else 0 end) / sum(qtdePedidoMeioPagamento) as pct_qtd_credit_card_pedido,
+  sum(case when descTipoPagamento='voucher' then qtdePedidoMeioPagamento else 0 end) / sum(qtdePedidoMeioPagamento) as pct_qtd_voucher_pedido,
+  sum(case when descTipoPagamento='debit_card' then qtdePedidoMeioPagamento else 0 end) / sum(qtdePedidoMeioPagamento) as pct_qtd_debit_card_pedido,
+
+  sum(case when descTipoPagamento='boleto' then vlPedidoMeioPagamento else 0 end) / sum(vlPedidoMeioPagamento) as pct_valor_boleto_pedido,
+  sum(case when descTipoPagamento='credit_card' then vlPedidoMeioPagamento else 0 end) / sum(vlPedidoMeioPagamento) as pct_valor_credit_card_pedido,
+  sum(case when descTipoPagamento='voucher' then vlPedidoMeioPagamento else 0 end) / sum(vlPedidoMeioPagamento) as pct_valor_voucher_pedido,
+  sum(case when descTipoPagamento='debit_card' then vlPedidoMeioPagamento else 0 end) / sum(vlPedidoMeioPagamento) as pct_valor_debit_card_pedido
+
+FROM tb_group
+
+GROUP BY 1
