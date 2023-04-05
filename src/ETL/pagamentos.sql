8133b647236c7d804c2eae1c8bf805949a8f6bc0
yonahirakawa <yonahirakawa>
2023-04-05 23:33:57 +0000

    ETL de pagamentos completo

--- src/ETL/pagamentos.sql
+++ src/ETL/pagamentos.sql
@@ -12,21 +12,47 @@ ORDER BY 1
 
 -- COMMAND ----------
 
-WITH tb_join (
+SELECT 
+  DISTINCT 
+      t1.idPedido, 
+      t2.idVendedor
+FROM silver.olist.pedido as t1
 
-  SELECT t2.*,
-         t3.idVendedor
+LEFT JOIN silver.olist.item_pedido as t2
+ON t1.idPedido = t2.idPedido
+
+WHERE t1.dtPedido < '2018-01-01'
+AND t1.dtPedido >= add_months('2018-01-01', -6)
+
+
+-- COMMAND ----------
+
+WITH tb_pedidos (
+  SELECT 
+    DISTINCT 
+        t1.idPedido, 
+        t2.idVendedor
   FROM silver.olist.pedido as t1
 
-  LEFT JOIN silver.olist.pagamento_pedido as t2
+  LEFT JOIN silver.olist.item_pedido as t2
   ON t1.idPedido = t2.idPedido
-
-  LEFT JOIN silver.olist.item_pedido as t3
-  ON t1.idPedido = t3.idPedido
 
   WHERE t1.dtPedido < '2018-01-01'
   AND t1.dtPedido >= add_months('2018-01-01', -6)
-  AND t3.idVendedor IS NOT NULL
+  AND idVendedor IS NOT NULL
+),
+
+
+tb_join (
+
+  SELECT 
+        t1.idVendedor,
+        t2.*         
+
+  FROM tb_pedidos AS t1
+
+  LEFT JOIN silver.olist.pagamento_pedido AS t2
+  ON t1.idPedido = t2.idPedido
 ),
 
 tb_group AS (
@@ -38,7 +64,9 @@ tb_group AS (
   FROM tb_join
   GROUP BY idVendedor, descTipoPagamento
   ORDER BY idVendedor, descTipoPagamento
-)
+),
+
+tb_summary AS (
 
 SELECT idVendedor,
   SUM(CASE WHEN descTipoPagamento = 'boleto' then qntPedidoMeioPagamento else 0 end) AS qnt_boleto_pedido,
@@ -62,7 +90,37 @@ SELECT idVendedor,
   SUM(CASE WHEN descTipoPagamento='debit_card' then vlPedidoMeioPagamento else 0 end) / sum(vlPedidoMeioPagamento) as pct_valor_debit_card_pedido
 
 FROM tb_group
-GROUP BY 1
+GROUP BY idVendedor
+),
+
+tb_cartao as (
+
+  SELECT idVendedor,
+         AVG(nrParcelas) AS avgQtdeParcelas,
+         PERCENTILE(nrParcelas, 0.5) AS medianQtdeParcelas,
+         MAX(nrParcelas) AS maxQtdeParcelas,
+         MIN(nrParcelas) AS minQtdeParcelas
+
+  FROM tb_join
+
+  WHERE descTipoPagamento = 'credit_card'
+
+  GROUP BY idVendedor
+
+)
+
+SELECT 
+       '2018-01-01' AS dtReference,
+       t1.*,
+       t2.avgQtdeParcelas,
+       t2.medianQtdeParcelas,
+       t2.maxQtdeParcelas,
+       t2.minQtdeParcelas
+
+FROM tb_summary as t1
+
+LEFT JOIN tb_cartao as t2
+ON t1.idVendedor = t2.idVendedor
 
 -- COMMAND ----------
 
