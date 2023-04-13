70445e2209b27ecea774f680178a7a0d761338f7
teocalvo2@gmail.com <teocalvo2@gmail.com>
2023-04-13 01:07:31 +0000

    Deploy modelo

--- src/ml/train.py
+++ src/ml/train.py
@@ -1,14 +1,21 @@
 # Databricks notebook source
-# MAGIC %pip install feature-engine
+# MAGIC %pip install feature-engine scikit-plot mlflow
 
 # COMMAND ----------
 
 from sklearn import model_selection
 from sklearn import tree
 from sklearn import pipeline
-import pandas as pd
+from sklearn import metrics
+from sklearn import ensemble
+
+import scikitplot as skplt
+
+import mlflow
 
 from feature_engine import imputation
+
+import pandas as pd
 
 pd.set_option('display.max_rows', 1000)
 
@@ -16,7 +23,6 @@ pd.set_option('display.max_rows', 1000)
 
 # DBTITLE 1,Sample Out Of Time
 ## SAMPLE
-
 df = spark.table("silver.analytics.abt_olist_churn").toPandas()
 
 # Base Out Of Time
@@ -32,10 +38,12 @@ df_train.head()
 
 var_identity = ['dtReference','idVendedor']
 target = 'flChurn'
+to_remove = ['qtdRecencia', target] + var_identity
 
 features = df.columns.tolist()
-features = list(set(features) - set(var_identity + [target]))
+features = list(set(features) - set(to_remove))
 features.sort()
+features
 
 # COMMAND ----------
 
@@ -71,30 +79,105 @@ missing_0 = ['medianQtdeParcelas',
 
 # COMMAND ----------
 
-# DBTITLE 1,Transform
-imputer_minus_100 = imputation.ArbitraryNumberImputer(arbitrary_number=-100,
-                                                      variables=missing_minus_100)
-
-imputer_0 = imputation.ArbitraryNumberImputer(arbitrary_number=0,
-                                              variables=missing_0)
+# DBTITLE 1,Define Experimento
+mlflow.set_experiment("/Users/teomewhy@gmail.com/olist-churn-teo")
 
 # COMMAND ----------
 
 # DBTITLE 1,Model
-# este é um modelo de árvore de decisão
-model = tree.DecisionTreeClassifier()
+with mlflow.start_run():
+
+    mlflow.sklearn.autolog()
+    mlflow.autolog()
+    
+    imputer_minus_100 = imputation.ArbitraryNumberImputer(arbitrary_number=-100,
+                                                          variables=missing_minus_100)
+
+    imputer_0 = imputation.ArbitraryNumberImputer(arbitrary_number=0,
+                                                  variables=missing_0)
+
+    # este é um modelo de árvore de decisão
+    model = ensemble.RandomForestClassifier(n_jobs=-1,
+                                            random_state=42)
+
+    params = {"min_samples_leaf": [5,10,20],
+              "n_estimators":[300,400,450, 500]}
+    
+    grid = model_selection.GridSearchCV(model, params, cv=3, verbose=3, scoring='roc_auc')
+    
+    # Criando o pipeline
+    model_pipeline = pipeline.Pipeline([("Imputer -100", imputer_minus_100),
+                                        ("Imputer 0", imputer_0),
+                                        ("Grid Search", grid),
+                                        ])  
+   
+    model_pipeline.fit(X_train, y_train)
+    
+    auc_train = metrics.roc_auc_score(y_train, model_pipeline.predict_proba(X_train)[:,1])
+    auc_test = metrics.roc_auc_score(y_test, model_pipeline.predict_proba(X_test)[:,1])
+    auc_oot = metrics.roc_auc_score(df_oot[target], model_pipeline.predict_proba(df_oot[features])[:,1])
+    
+    metrics_model = {"auc_train": auc_train,
+                    "auc_test": auc_test,
+                    "auc_oot": auc_oot}
+
+    mlflow.log_metrics(metrics_model)
 
 # COMMAND ----------
 
-model_pipeline = pipeline.Pipeline([("Imputer -100", imputer_minus_100),
-                                    ("Imputer 0", imputer_0),
-                                    ("Decision Tree", model),
-                                    ])
+pd.DataFrame(grid.cv_results_).sort_values(by='rank_test_score')
 
 # COMMAND ----------
 
-model_pipeline.fit(X_train, y_train)
+grid.best_estimator_
 
 # COMMAND ----------
 
-model_pipeline.predict(X_test)
+skplt.metrics.plot_roc(y_train, probas)
+
+# COMMAND ----------
+
+skplt.metrics.plot_ks_statistic(y_train, probas)
+
+# COMMAND ----------
+
+skplt.metrics.plot_lift_curve(y_train, probas)
+
+# COMMAND ----------
+
+df_teo = pd.DataFrame()
+df_teo['target'] = y_train
+df_teo['proba'] = probas[:,1]
+df_teo = df_teo.sort_values(by='proba')
+df_teo.tail(100)['target'].mean() / df_teo['target'].mean()
+
+# COMMAND ----------
+
+probas_test = model_pipeline.predict_proba(X_test)
+
+# COMMAND ----------
+
+skplt.metrics.plot_roc(y_test, probas_test)
+
+# COMMAND ----------
+
+skplt.metrics.plot_ks_statistic(y_test, probas_test)
+
+# COMMAND ----------
+
+probas_oot = model_pipeline.predict_proba(df_oot[features])
+
+# COMMAND ----------
+
+skplt.metrics.plot_roc(df_oot[target], probas_oot)
+
+# COMMAND ----------
+
+skplt.metrics.plot_ks_statistic(df_oot[target], probas_oot)
+
+# COMMAND ----------
+
+fs_importance = model_pipeline[-1].feature_importances_
+fs_cols = model_pipeline[:-1].transform(X_train.head(1)).columns.tolist()
+
+pd.Series(fs_importance, index=fs_cols).sort_values(ascending=False)
