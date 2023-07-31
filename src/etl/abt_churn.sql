5d1e75fcad46bf23542c7ca10d54eede63241937
luizroberto.zl15@gmail.com <luizroberto.zl15@gmail.com>
2023-07-31 20:42:36 +0000

    Ajuste query

--- src/etl/abt_churn.sql
+++ src/etl/abt_churn.sql
@@ -31,18 +31,23 @@ FROM
 LEFT JOIN 
       sandbox.analystics_churn_model.fs_vendedor_avaliacao t2
       ON t1.idVendedor = t2.idVendedor
+      AND t1.DateRef = t2.DateRef
 LEFT JOIN 
       sandbox.analystics_churn_model.fs_vendedor_produto t3
       ON t1.idVendedor = t3.idVendedor 
+      AND t1.DateRef = t3.DateRef
 LEFT JOIN 
       sandbox.analystics_churn_model.fs_vendedor_entrega  t4
       ON t1.idVendedor = t4.idVendedor
+      AND t1.DateRef = t4.DateRef
 LEFT JOIN 
       sandbox.analystics_churn_model.fs_vendedor_cliente t5
       ON t1.idVendedor = t5.idVendedor
+      AND t1.DateRef = t5.DateRef
 LEFT JOIN 
       sandbox.analystics_churn_model.fs_vendedor_pagamento t6
       ON t1.idVendedor = t6.idVendedor
+      AND t1.DateRef = t6.DateRef
 LEFT JOIN 
       tbl_active  t7
             ON t1.idVendedor = t7.idVendedor
